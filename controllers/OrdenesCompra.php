c5ab818005207e17591e65ac02b3c90bfd23b6b6
Julio Marroquin <juliomarroquin1993@hotmail.com>
2025-11-30 20:08:18 -0600

    FixOrdenes

--- controllers/OrdenesCompra.php
+++ controllers/OrdenesCompra.php
@@ -117,7 +117,7 @@ class OrdenesCompra extends Controller{
     public function listado(){
         $data['title'] = 'Listado de Órdenes de Compra';
         $data['script'] = 'listadoOrdenesCompra.js';
-        $data['ordenes'] = $this->model->getOrdenes();
+       // $data['ordenes'] = $this->model->getOrdenes();
         $this->views->getView('ordenesCompra', 'listadoOrdenes', $data);
     }
 

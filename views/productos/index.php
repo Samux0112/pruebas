327dfda2069173426a0f46b341b5f84fb6664fc8
Julio Marroquin <juliomarroquin1993@hotmail.com>
2025-12-27 23:37:48 -0600

    vista productos

--- views/productos/index.php
+++ views/productos/index.php
@@ -22,9 +22,8 @@
         <nav>
             <div class="nav nav-tabs" id="nav-tab" role="tablist">
                 <button class="nav-link active" id="nav-productos-tab" data-bs-toggle="tab" data-bs-target="#nav-productos" type="button" role="tab" aria-controls="nav-productos" aria-selected="true">Productos</button>
-                <?php if (!in_array($_SESSION['rol_usuario'], ['VENDEDOR', 'VENDEDOR ADMINISTRATIVO'])) { ?>
                 <button class="nav-link" id="nav-nuevo-tab" data-bs-toggle="tab" data-bs-target="#nav-nuevo" type="button" role="tab" aria-controls="nav-nuevo" aria-selected="false">Nuevo</button>
-                <?php } ?>
+
             </div>
         </nav>
         <div class="tab-content" id="nav-tabContent">

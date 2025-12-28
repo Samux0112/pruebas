dd126b41b88103b7291f9110eb7403b4062fc04d
Julio Marroquin <juliomarroquin1993@hotmail.com>
2025-12-28 17:17:08 -0600

    AGREGANDO PERMISOS PARA VER PDF DE ORDEN DE COMPRA

--- views/ordenesCompra/listadoOrdenes.php
+++ views/ordenesCompra/listadoOrdenes.php
@@ -44,7 +44,12 @@ require 'views/templates/header.php';
                                                     <?php endif; ?>
                                                 </td>
                                                 <td>
+                                                    <?php 
+                                                    $rolesPermitidos = ['ADMINISTRADOR', 'SUPERVISOR', 'CONTADOR', 'INVENTARIO', 'VENDEDOR ADMINISTRATIVO'];
+                                                    if (isset($_SESSION['rol_usuario']) && in_array($_SESSION['rol_usuario'], $rolesPermitidos)) {
+                                                    ?>
                                                     <a href="<?php echo ($orden['estado'] === 'aprobado') ? (BASE_URL . 'ordenesCompra/generarPDF/' . $orden['id']) : '#'; ?>" target="_blank" class="btn btn-primary btn-sm<?php echo ($orden['estado'] !== 'aprobado') ? ' disabled' : ''; ?>" tabindex="-1" aria-disabled="<?php echo ($orden['estado'] !== 'aprobado') ? 'true' : 'false'; ?>">Ver PDF</a>
+                                                    <?php } ?>
                                                     <button type="button" class="btn btn-success btn-sm ms-1 btnCompletarCompra" data-id="<?php echo htmlspecialchars($orden['id']); ?>" <?php echo ($orden['estado'] !== 'aprobado') ? 'disabled' : ''; ?>>Completar compra</button>
                                                     <?php if (isset($_SESSION['rol_usuario']) && in_array($_SESSION['rol_usuario'], ['ADMINISTRADOR', 'SUPERVISOR','CONTADOR'])): ?>
                                                         <button type="button" class="btn btn-warning btn-sm ms-1 btnAutorizarOrden" data-id="<?php echo htmlspecialchars($orden['id']); ?>">Autorizar</button>
